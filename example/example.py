2e8d4cd10e39391e08452e32da3e181411e153f6
Tom <tbowyer@outlook.com>
2020-03-20 22:46:43 -0500

    adding CRS to example

--- example/example.py
+++ example/example.py
@@ -1,4 +1,4 @@
-# Pull from https://waf.ninja/modsecurity-rules-verification/
+# Started from https://waf.ninja/modsecurity-rules-verification/
 import json
 import os
 
@@ -9,20 +9,14 @@ def modsec(event, context):
 
     modsec = ModSecurity()
     rules = Rules()
-    rules.load('''SecRuleEngine On
-    SecDebugLog /tmp/debug.log
-    SecDebugLogLevel 9
-    SecRule TX:EXECUTING_PARANOIA_LEVEL "@lt 1" "id:941011,phase:1,pass,nolog,skipAfter:END-REQUEST-941-APPLICATION-ATTACK-XSS"
-    SecRule TX:EXECUTING_PARANOIA_LEVEL "@lt 1" "id:941012,phase:2,pass,nolog,skipAfter:END-REQUEST-941-APPLICATION-ATTACK-XSS"
-    SecRule REQUEST_COOKIES|!REQUEST_COOKIES:/__utm/|REQUEST_COOKIES_NAMES|REQUEST_HEADERS:User-Agent|REQUEST_HEADERS:Referer|ARGS_NAMES|ARGS|XML:/* "@rx (?i)[<＜]script[^>＞]*[>＞][\s\S]*?" "id:1,phase:2,deny,deny,t:none,status:403"
-    ''')
+    rules.loadFromUri("/opt/lib/modsec.conf")
     
     ret = rules.getParserError()
     if ret:
         print('Unable to parse rule: %s' % ret)
     
     transaction = Transaction(modsec, rules, None)
-    transaction.processURI("/docs/index.html?a=<script>alert(1)</script>&q=test", "GET", "1.1")
+    transaction.processURI("/docs/index.html?a=SELECT * FROM", "POST", "2.0")
     transaction.processRequestHeaders()
     transaction.processRequestBody()
     intervention = ModSecurityIntervention()
